04bf553dfe925d1658f8b870172c591bf97a9da9
faizadli <mochfaizadli@gmail.com>
2021-01-29 11:01:28 +0700

    first commit

--- NavigationUI/ContentView.swift
+++ NavigationUI/ContentView.swift
@@ -9,13 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home: View {
+    var body: some View{
+        NavigationView{
+            Text("Hello ini content")
+            
+                .navigationBarItems(
+                    
+                    leading:
+                        HStack{
+                            Button(action: {print("Hello World")}){
+                                Image("youtube_logo")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .frame(width: 90, height: 20)
+                            }
+                        },
+                    
+                    trailing:
+                        HStack(spacing: 20){
+                            Button(action: {print("Hello Button")}){
+                                Image(systemName: "tray")
+                                    .foregroundColor(Color.black)
+                            }
+                            
+                            Button(action: {print("Hello Button")}){
+                                Image(systemName: "video.fill")
+                                    .foregroundColor(Color.black)
+                            }
+                            
+                            Button(action: {print("Hello Button")}){
+                                Image(systemName: "magnifyingglass")
+                                    .foregroundColor(Color.black)
+                            }
+                            
+                            Button(action: {print("Hello Button")}){
+                                Image("profile")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .clipShape(Circle())
+                            }
+                        }
+                )
+        }
     }
 }
